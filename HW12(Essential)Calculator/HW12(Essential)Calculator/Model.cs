7c2bafd42ae4fec831e1bdcb26ab6086c5716820
daniuxa <daniakroos8@gmail.com>
2022-07-24 23:18:59 +0300

    Added 12 lesson calculator

---
+++ HW12(Essential)Calculator/HW12(Essential)Calculator/Model.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12_Essential_Calculator
+{
+    internal class Model
+    {
+        public double Calculate(string NumStr)
+        {
+            double result = 0;
+            string[] ParseChars = NumStr.Split('+', '-', '*', '/');
+            if (ParseChars.Length == 2)
+            {
+                if (NumStr.Contains("+"))
+                {
+                    result = double.Parse(ParseChars[0]) + double.Parse(ParseChars[1]);
+                }
+                else if (NumStr.Contains("-"))
+                {
+                    result = double.Parse(ParseChars[0]) - double.Parse(ParseChars[1]);
+                }
+                else if (NumStr.Contains("*"))
+                {
+                    result = double.Parse(ParseChars[0]) * double.Parse(ParseChars[1]);
+                }
+                else if (NumStr.Contains("/"))
+                {
+                    result = double.Parse(ParseChars[0]) / double.Parse(ParseChars[1]);
+                }
+            }
+            else
+            {
+                throw new Exception("Must be only 2 operands sepparated by (+, -, *, /)");
+            }
+            return result;
+        }
+    }
+}
---
+++ HW12(Essential)Calculator/HW12(Essential)Calculator/Presenter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12_Essential_Calculator
+{
+    internal class Presenter
+    {
+        MainWindow MainWindow { get; set; }
+        Model Model { get; set; }
+        public Presenter(MainWindow mainWindow)
+        {
+            MainWindow = mainWindow;
+            Model = new Model();
+            MainWindow.CalculateBtnPressed += MainWindow_CalculateBtnPressed;
+        }
+
+        private void MainWindow_CalculateBtnPressed(object sender, EventArgs e)
+        {
+            try
+            {
+                MainWindow.ResultBlock.Text = "= " + Model.Calculate(MainWindow.TextBoxField.Text);
+            }
+            catch (Exception ex)
+            {
+                MainWindow.ErrorMsg(ex.Message);
+            }
+        }
+    }
+}
