d46281b0379557c8b5d83201a279afb98277c282
daniuxa <daniakroos8@gmail.com>
2022-08-09 19:10:18 +0300

    Added 9th lesson

---
+++ HW(Professional)/HW9(Professional)/HW9(Professional)/MyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW9_Professional_
+{
+    internal class MyClass : IDisposable
+    {
+        int[] vs = new int[100000000];
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Console.WriteLine(GC.GetTotalMemory(false)); 
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) 
+                return;
+            if (disposing)
+            {
+                // Освобождаем управляемые ресурсы
+            }
+            // освобождаем неуправляемые объекты
+            disposed = true;
+        }
+        ~MyClass()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ HW(Professional)/HW9(Professional)/HW9(Professional)/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Создайте свой класс, объекты которого будут занимать много места в памяти (например, в
+коде класса будет присутствовать большой массив) и реализуйте для этого класса,
+формализованный шаблон очистки.*/
+
+using HW9_Professional_;
+
+GC.GetTotalAllocatedBytes();
+Console.WriteLine(GC.GetTotalMemory(false) / (1024*1024));
+MyClass myClass1 = new MyClass();
+Console.WriteLine(GC.GetTotalMemory(false) / (1024 * 1024));
+MyClass myClass2 = new MyClass();
+Console.WriteLine(GC.GetTotalMemory(false) / (1024 * 1024));
+MyClass myClass3 = new MyClass();
+Console.WriteLine(GC.GetTotalMemory(false) / (1024 * 1024));
+Console.ReadKey();
