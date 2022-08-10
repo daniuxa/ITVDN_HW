89d23dc82e79c56eb0e7d691450be96ebeb8ea93
daniuxa <daniakroos8@gmail.com>
2022-08-10 14:37:28 +0300

    Added 10th lesson

---
+++ HW(Professional)/HW10(Professional)/HW10(Professional)/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Реализуйте шаблон NVI в собственной иерархии наследования.*/
+
+MyClass1 myClass = new MyClass1();
+myClass.DoWork();
+
+public class MyClass1
+{
+    public void DoWork()
+    {
+        Console.WriteLine("This is interface for user, to use my class and logic is hidden in others methods");
+        PreDoWork();
+        CoreDoWork();
+    }
+    protected virtual void CoreDoWork()
+    {
+        Console.WriteLine("Base work which my class do");
+    }
+    private void PreDoWork()
+    {
+        Console.WriteLine("I need to remake something in base class, this why I create this class");
+    }
+}
+
+public class MyClass2 : MyClass1
+{
+    protected override void CoreDoWork()
+    {
+        Console.WriteLine("New work which my class do for user");
+    }
+}
+
+
---
+++ HW(Professional)/HW10(Professional)/SecondTask/Program.cs
@@ -0,0 +1,52 @@
+﻿/*Напишите небольшую программу на языке C#, представляющую собой абстрактную
+реализацию данного шаблона.*/
+
+Programmer programmer = new Programmer();
+Admin admin = new Admin();
+
+programmer.Work();
+admin.Work();
+
+public class Programmer : Worker
+{
+
+    public void MakeCode()
+    {
+        Console.WriteLine("Proging");
+    }
+
+    public override void Work()
+    {
+        Console.WriteLine("Programmer work");
+    }
+
+    public override void GetSalary()
+    {
+        Console.WriteLine("5000$");
+    }
+}
+
+
+public class Admin : Worker
+{
+    public void CheckStaff()
+    {
+        Console.WriteLine("Checking");
+    }
+
+    public override void Work()
+    {
+        Console.WriteLine("Admin work");
+    }
+
+    public override void GetSalary()
+    {
+        Console.WriteLine("2000$");
+    }
+}
+
+public abstract class Worker
+{
+    public abstract void Work();
+    public abstract void GetSalary();
+}
