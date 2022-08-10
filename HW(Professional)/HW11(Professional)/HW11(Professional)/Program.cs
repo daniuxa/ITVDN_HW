772d4e5a670c42b05e9fd42b1604d4f277216e38
daniuxa <daniakroos8@gmail.com>
2022-08-11 01:22:58 +0300

    Added 11th lesson

---
+++ HW(Professional)/HW11(Professional)/HW11(Professional)/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Используя конструкции блокировки, создайте метод, который будет в цикле for (допустим, на 10
+итераций) увеличивать счетчик на единицу и выводить на экран счетчик и текущий поток.
+Метод запускается в трех потоках. Каждый поток должен выполниться поочередно, т.е. в
+результате на экран должны выводиться числа (значения счетчика) с 1 до 30 по порядку, а не в
+произвольном порядке.*/
+
+int counter = 0;
+object locker = new object();
+
+void Method()
+{
+    lock (locker)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Counter - {++counter} --- Thread - {Thread.CurrentThread.GetHashCode()}");
+        }
+
+    }
+}
+
+Thread[] threads = new Thread[3];
+for (int i = 0; i < threads.Length; i++)
+{
+    threads[i] = new Thread(Method);
+    threads[i].Start();
+}
+
+
---
+++ HW(Professional)/HW11(Professional)/SecondTask/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Создайте консольное приложение, которое в различных потоках сможет получить доступ к 2-м
+файлам. Считайте из этих файлов содержимое и попытайтесь записать полученную
+информацию в третий файл. Чтение/запись должны осуществляться одновременно в каждом
+из дочерних потоков. Используйте блокировку потоков для того, чтобы добиться корректной
+записи в конечный файл.*/
+
+object locker = new Object();
+
+void ReadFromFile(string FileName)
+{
+    string Text = File.ReadAllText(FileName);
+    lock (locker)
+    {
+        using (StreamWriter writer = new StreamWriter("FileIn.txt", true))
+        {
+            writer.WriteLine(Text);
+        }
+    }
+}
+
+Thread thread1 = new Thread(() => { ReadFromFile("FileOut1.txt"); });
+Thread thread2 = new Thread(() => { ReadFromFile("FileOut2.txt"); });
+
+thread1.Start();
+thread2.Start();
