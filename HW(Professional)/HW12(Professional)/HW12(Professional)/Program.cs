f12a88e17e0c9915a46d350850af36032299af01
daniuxa <daniakroos8@gmail.com>
2022-08-12 02:13:34 +0300

    Added 12 lesson

---
+++ HW(Professional)/HW12(Professional)/HW12(Professional)/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Создайте Semaphore, осуществляющий контроль доступа к ресурсу из нескольких потоков.
+Организуйте упорядоченный вывод информации о получении доступа в специальный *.log
+файл.*/
+
+
+Semaphore semaphore = new Semaphore(2, 4);
+Thread[] threads = new Thread[10];
+object obj = new object();
+for (int i = 0; i < threads.Length; i++)
+{
+    threads[i] = new Thread(() => { MakeConnection(semaphore); });
+    threads[i].Start();
+}
+
+void MakeConnection(Semaphore sem)
+{
+    sem.WaitOne();
+    try
+    {
+        //lock (obj)
+        {
+            using (FileStream fileStream = new FileStream("Log.log", FileMode.Open, FileAccess.Read))
+            {
+                Console.WriteLine($"Thread - {Thread.CurrentThread.GetHashCode()} made a connection");
+                Thread.Sleep(1500);
+            }
+            Console.WriteLine($"Thread - {Thread.CurrentThread.GetHashCode()} close a connection");
+        }
+        
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }    
+    sem.Release();
+}
---
+++ HW(Professional)/HW12(Professional)/ThirdTask/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Создайте приложение, которое может быть запущено только в одном экземпляре (используя
+именованный Mutex).*/
+bool createdNew;
+Mutex mutex = new Mutex(false, "MyMutex", out createdNew);
+
+if (!createdNew)
+{
+    return;
+}
+
+mutex.WaitOne();
+Console.WriteLine("Working....");
+Thread.Sleep(50000);
+
+
+mutex.ReleaseMutex();
+
