0af637f8f740c14c5ee94344b845d1344dabd51e
daniuxa <daniakroos8@gmail.com>
2022-08-13 02:18:01 +0300

    Added 14th lesson

---
+++ HW(Professional)/HW14(Professional)/HW14(Professional)/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Создайте массив чисел размерностью в 1 000 000 или более. Используя генератор случайных
+чисел, проинициализируйте этот массив значениями. Создайте PLINQ запрос, который
+позволит получить все нечетные числа из исходного массива.*/
+
+int[] vs = new int[1000000];
+Random random = new Random();
+Parallel.For(0, vs.Length, i => vs[i] = random.Next(0, 100));
+
+//ParallelQuery<int> parallelVs = vs.AsParallel().Where(x => x % 2 == 0);
+ParallelQuery<int> parallelVs = from x in vs.AsParallel()
+                                where x % 2 == 0
+                                select x;
+
+Parallel.ForEach(parallelVs, i => Console.Write(i + "  "));
---
+++ HW(Professional)/HW14(Professional)/SecondTask/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Создайте два метода, которые будут выполняться в рамках параллельных задач. Организуйте
+вызов этих методов при помощи Invoke таким образом, чтобы основной поток программы
+(метод Main) не остановил свое выполнение.*/
+
+Task task = new Task( () => Parallel.Invoke(Method1, Method2));
+task.Start();
+
+for (int i = 0; i < 80; i++)
+{
+    Thread.Sleep(100);
+    Console.Write("M ");
+}
+
+
+
+
+
+
+void Method1()
+{
+    for (int i = 0; i < 80; i++)
+    {
+        Thread.Sleep(100);
+        Console.Write("1 ");
+    }
+}
+
+void Method2()
+{
+    for (int i = 0; i < 80; i++)
+    {
+        Thread.Sleep(100);
+        Console.Write("2 ");
+    }
+}
