7cbfc4108dfa1638aa78eef240f77044d665f28d
daniuxa <daniakroos8@gmail.com>
2022-08-16 01:40:43 +0300

    Added functional prodramming lesson

---
+++ HW(Professional)/HWFunc(Professional)/HWFunc(Professional)/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Создайте метод, который позволит выполнять над коллекцией целых чисел простейшие
+преобразования, возвращая в качестве результата новую коллекцию данных.
+Используя возможности функционального программирования, выполните над коллекцией
+следующие операции:
+- Увеличьте каждое значение в 2 раза
+- Замените каждое нечетное значение ближайшим четным (по возрастанию)*/
+
+
+List<int> vs = new List<int>();
+vs.Add(0);
+vs.Add(1);
+vs.Add(2);
+vs.Add(3);
+vs.Add(4);
+
+IEnumerable<int> enumerable = vs.Method(0,
+                            (col, cur) => { return cur % 2 == 0 ? col[cur] * 2 : col[cur + 1] * 2; },
+                            (col, cur) => cur > col.Count - 1,
+                            cur => cur + 1);
+foreach (var item in enumerable)
+{
+    Console.WriteLine(item);
+}
+
+static class Extension
+{
+    public static IEnumerable<int> Method <TCollection, TCursor>(this TCollection collection,
+                                                                                         TCursor cursor,
+                                                                                         Func<TCollection, TCursor, int> getCurrent,
+                                                                                         Func<TCollection, TCursor, bool> isFinished,
+                                                                                         Func<TCursor, TCursor> advanceCursor)
+    {
+        while (!isFinished(collection, cursor))
+        {
+            yield return getCurrent(collection, cursor);
+            cursor = advanceCursor(cursor);
+        }
+    }
+}
+
+
---
+++ HW(Professional)/HWFunc(Professional)/SecondTask/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Используя возможности класса Expression, сформируйте выражение, реализующее
+следующее математическое выражение: y = 2 * (x – 3) + x – 4.*/
+
+using System.Linq.Expressions;
+
+ParameterExpression x = Expression.Parameter(typeof(int), "x");
+
+var cons1 = Expression.Constant(2);
+var cons2 = Expression.Constant(3);
+var cons3 = Expression.Constant(4);
+
+var fst = Expression.Subtract(x, cons2);
+var snd = Expression.Multiply(fst, cons1);
+var trd = Expression.Add(snd, x);
+var frth = Expression.Subtract(trd, cons3);
+
+var expression = Expression.Lambda<Func<int, int>>(frth, x);
+Console.WriteLine(expression);
+
+Func<int, int> func = expression.Compile();
+
+Console.WriteLine(func(3));
