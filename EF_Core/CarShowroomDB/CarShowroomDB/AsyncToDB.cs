4ff27e5755cc831871ff3db00376ea1621808047
daniuxa <daniakroos8@gmail.com>
2022-12-20 16:58:21 +0200

    Added multithreading for task N4

---
+++ EF_Core/CarShowroomDB/CarShowroomDB/AsyncToDB.cs
@@ -0,0 +1,30 @@
+﻿using CarShowroomDbData;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroomDB
+{
+    public class AsyncToDB
+    {
+        private DbContextOptions options;
+        public AsyncToDB(DbContextOptions options)
+        {
+            this.options = options;
+        }
+        public async Task AsyncAdd()
+        {
+            using (CarShowroomContext context = new CarShowroomContext(options))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    await context.Clients.AddAsync(new CarShowroomDomain.Client { LName = "Client " + i });
+                    await context.SaveChangesAsync();
+                }         
+            }
+        }
+    }
+}
